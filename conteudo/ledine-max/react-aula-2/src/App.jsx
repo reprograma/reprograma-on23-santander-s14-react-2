9a89ec45b29fddca2dcf940c5cc36a7502848476
ledinemax <ledinemax@gmail.com>
2023-06-26 20:52:55 -0300

    Aula sabado

---
+++ conteudo/ledine-max/react-aula-2/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Filmes from './componentes/Requisição'
+
+
+function App() {
+
+  return (
+   <Filmes/>
+  )
+}
+
+export default App
---
+++ conteudo/ledine-max/react-aula-2/src/componentes/Cards.jsx
@@ -0,0 +1,24 @@
+// import filmes from '../data/Ghibli'
+
+// function Cards(){
+//     return (
+//         <div className='cards_list'>
+//             {ghibli.map(filme => 
+//                 <div className='cards'>
+//                     <img key={filme.id} alt={filme.title} src={filme.image}/>
+//                     <h1>
+//                         {filme.title}
+//                     </h1>
+//                     <p>
+//                         {filme.original_title}
+//                     </p>
+//                 </div>
+//             )
+                
+//             }
+//         </div>
+//     )
+// }
+
+
+// export default Cards
